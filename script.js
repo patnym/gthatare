5728029386d0628a111d3f3da0d59ac8325abed8
Sebastian Håkansson <sebastian.hakansson@axakon.se>
2020-03-06 14:29:59 +0100

    added a new gt hater to the roster with a really cool frame thing and every changing pictures

--- script.js
+++ script.js
@@ -10,21 +10,26 @@ function setHateIndex(veryRandomGtArray) {
   document.getElementById(
     "gt-hater3"
   ).textContent = veryRandomGtArray[2].toString();
+  document.getElementById(
+    "gt-hater4"
+  ).textContent = veryRandomGtArray[3].toString();
 }
 
 function getInitialHateIndex() {
   const gtRandom1 = Math.floor(Math.random() * 100);
   const gtRandom2 = Math.floor(Math.random() * 100);
   const gtRandom3 = Math.floor(Math.random() * 100);
-  currentHateIndexes = [gtRandom1, gtRandom2, gtRandom3];
-  return [gtRandom1, gtRandom2, gtRandom3];
+  const gtRandom4 = Math.floor(Math.random() * 100);
+  currentHateIndexes = [gtRandom1, gtRandom2, gtRandom3, gtRandom4];
+  return [gtRandom1, gtRandom2, gtRandom3, gtRandom4];
 }
 
 function updateHateIndex(hateIndexes) {
   const newHateIndexes = [];
-  newHateIndexes[0] = hateIndexes[0] - Math.floor(Math.random() * 10) - 9;
-  newHateIndexes[1] = hateIndexes[1] - Math.floor(Math.random() * 10) - 9;
-  newHateIndexes[2] = hateIndexes[2] + Math.floor(Math.random() * 10) - 9;
+  newHateIndexes[0] = hateIndexes[0] + Math.floor(Math.random() * 30);
+  newHateIndexes[1] = hateIndexes[1] + Math.floor(Math.random() * 30);
+  newHateIndexes[2] = hateIndexes[2] + Math.floor(Math.random() * 30);
+  newHateIndexes[3] = hateIndexes[3] + Math.floor(Math.random() * 30);
   return newHateIndexes;
 }
 
@@ -38,32 +43,46 @@ function setUpdatedHateIndexes() {
   move(newHateIndexes, 0);
   move(newHateIndexes, 1);
   move(newHateIndexes, 2);
+  move(newHateIndexes, 3);
   catsopatsotimeu();
-
 }
 
 setInitialHateIndex();
 setInterval(setUpdatedHateIndexes, 1000);
 
 function cat(cat) {
-   var result           = '';
-   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-   var charactersLength = characters.length;
-   for ( var i = 0; i < cat; i++ ) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-   }
-   return result;
+  var result = "";
+  var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  var charactersLength = characters.length;
+  for (var i = 0; i < cat; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
 }
 
 function move(newHateIndexes, id) {
-    var elem = document.getElementById('bar-' + id +'').style;
-        elem.width = newHateIndexes[id] + "%";
-        elem.backgroundColor = "rgb(" + Math.floor(Math.random() * 255) +"," + Math.floor(Math.random() * 255) + "," +  Math.floor(Math.random() * 255) +")";
+  var elem = document.getElementById("bar-" + id + "").style;
+  elem.width = newHateIndexes[id] + "%";
+  elem.backgroundColor =
+    "rgb(" +
+    Math.floor(Math.random() * 255) +
+    "," +
+    Math.floor(Math.random() * 255) +
+    "," +
+    Math.floor(Math.random() * 255) +
+    ")";
 }
 
-function catsopatsotimeu(){
-  var catt = document.getElementById('cat');
+function catsopatsotimeu() {
+  var catt = document.getElementById("cat");
   const catcat = Math.floor(Math.random() * 255);
   catt.textContent = cat(1);
-  catt.style.color = "rgb(" + Math.floor(Math.random() * 255) +"," + Math.floor(Math.random() * 255) + "," +  Math.floor(Math.random() * 255) +")";
+  catt.style.color =
+    "rgb(" +
+    Math.floor(Math.random() * 255) +
+    "," +
+    Math.floor(Math.random() * 255) +
+    "," +
+    Math.floor(Math.random() * 255) +
+    ")";
 }
